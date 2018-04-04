3e22fb81b36f78710f48be5c78d96bae88b4f3e4
Danuta <danprzy@gmail.com>
2018-04-04 18:02:06 +0200

    Add task 9-2

---
+++ arrays/scripts.js
@@ -0,0 +1,14 @@
+var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
+var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
+var allNames = femaleNames.concat(maleNames);
+
+var newName = 'Ola'
+
+if (allNames.indexOf(newName) === -1) {
+    var x = allNames.push(newName);
+    document.write('Dodano imię: ' + newName);
+} else {
+    document.write('Imię ' + newName +' znajduje się już w tablicy');
+}
+
+console.log(allNames);
