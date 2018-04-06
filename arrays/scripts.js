179837dd189317616c500392dd932f173351c0c3
Danuta <danprzy@gmail.com>
2018-04-06 11:39:15 +0200

    Change function push

--- arrays/scripts.js
+++ arrays/scripts.js
@@ -2,10 +2,10 @@ var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
 var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
 var allNames = femaleNames.concat(maleNames);
 
-var newName = 'Ola'
+var newName = 'Ola';
 
 if (allNames.indexOf(newName) === -1) {
-    var x = allNames.push(newName);
+    allNames.push(newName);
     document.write('Dodano imię: ' + newName);
 } else {
     document.write('Imię ' + newName +' znajduje się już w tablicy');
