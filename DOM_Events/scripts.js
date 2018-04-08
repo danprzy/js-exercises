d3e540bb5e0369c6d9a61093aaae823963a23bae
Danuta <danprzy@gmail.com>
2018-04-08 21:24:00 +0200

    Add task 9-6

---
+++ DOM_Events/scripts.js
@@ -0,0 +1,11 @@
+
+var list = document.getElementById('list');
+var add = document.getElementById('addElem');
+
+add.addEventListener('click', function() {
+  var element = document.createElement('li');
+  var liNumber = document.getElementsByTagName('li').length;
+  element.innerHTML = 'item ' + liNumber; 
+  list.appendChild(element);
+});
+
