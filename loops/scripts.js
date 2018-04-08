66da54d043b5ffdce96dd3a5565dd6f789efbde9
Danuta <danprzy@gmail.com>
2018-04-08 21:31:20 +0200

    Add task 9-4 incomplete

---
+++ loops/scripts.js
@@ -0,0 +1,39 @@
+ 
+//połowa drzewka
+function drawTree (treeRow) {
+	for ( var i = 0 ; i < treeRow ; i++) {
+
+	  	var star = '';
+	  	for ( var j = 0 ; j <= i ; j++) {
+	  	star += '* ';
+	 	}
+	 console.log(star);
+	}
+}
+
+drawTree (5);
+
+
+/* zadanie rozszerzone,cała choinka - działa bez gornej gwiazdki
+function drawTree (treeRow) {
+//debugger
+	for ( var i = 0 ; i < treeRow ; i++) {
+		var space = '-';
+		for (var s = treeRow - i; s > 0 ; s--){
+	  	space += '-';
+	 	}
+	  	var star = '';
+	  	for ( var j = 0 ; j <= i ; j++) {
+	  	star += '**';
+	 	}
+	 console.log(space + star);
+	}
+}
+
+drawTree (5);
+*/
+
+
+
+
+
