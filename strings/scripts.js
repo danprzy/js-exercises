60d935d787ebc9d8435866aad1cabb2e747cafa1
Danuta <danprzy@gmail.com>
2018-04-17 12:56:56 +0200

    Add new version with slice and unshift methods

--- strings/scripts.js
+++ strings/scripts.js
@@ -1,5 +1,15 @@
 
-// Change first word 
+// Change first word - version A
+var text = 'Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.';
+var textArray = text.split(' ');
+var dinosaur = 'triceratops';
+var textArrayModify = textArray.slice(1);
+textArrayModify.unshift(dinosaur.toUpperCase());
+var textArrayToString = textArrayModify.join(' ');
+console.log(textArrayToString.slice(0, textArrayToString.length/2));
+
+/*
+// Change first word - version B
 var text = 'Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.';
 var textArray = text.split(' ');
 var dinosaur = 'triceratops';
@@ -8,8 +18,7 @@ var textArrayToString = textArray.join(' ');
 console.log(textArrayToString.slice(0, textArrayToString.length/2));
 
 
-// Change word Velociraptor
-/*
+// Change word Velociraptor - version C
 var text = 'Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.';
 var dinosaur = 'triceratops';
 var newText = text.replace('Velociraptor', dinosaur.toUpperCase())
